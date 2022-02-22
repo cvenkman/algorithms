ff37cd60a5e8e02cb762d41e47da3e7f06a9f4e0
Carlos Venkman <cvenkman@si-f3.kzn.21-school.ru>
2022-02-22 16:05:54 +0300

    add random array to main

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -1,6 +1,7 @@
 package BinarySearch;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class BinarySearch {
     private static final int NO_ELEMENT_ERROR = -1;
@@ -13,7 +14,6 @@ public class BinarySearch {
     public static int getKeyIndexWithBinarySearch(int key, int[] array) throws NullPointerException{
         if (array == null || array.length == 0)
             throw new NullPointerException("Honey, have you been forgetting array again?");
-        Arrays.sort(array);
         int firstIndex = 0;
         int lastIndex = array.length - 1;
         if (key < array[firstIndex] || key > array[lastIndex])
@@ -33,23 +33,35 @@ public class BinarySearch {
     }
 
     public static void main(String[] args) {
-        int[] array = {89, 57, 91, 47, 95, 3, 27, 22, 67, 99};
+        final int ARRAY_SIZE = 20;
+        Random random = new Random();
+        int[] array = new int[ARRAY_SIZE];
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            array[i] = random.nextInt();
+        }
+        Arrays.sort(array);
+
         try {
-            /*
-                // binarySearch from Arrays class:
-                Arrays.sort(array);
-                int index = Arrays.binarySearch(array, 67);
-                System.out.println(67 + " index: " + index);
-            */
-            int index = getKeyIndexWithBinarySearch(67, array);
-            System.out.println(67 + " index: " + index);
+            System.out.println("--- Search array[6]");
+            int index = getKeyIndexWithBinarySearch(array[6], array);
+            System.out.println("my binary search: " + array[6] + " index: " + index);
+            index = Arrays.binarySearch(array, array[6]);
+            System.out.println("array class binary search: " + array[6] + " index: " + index);
 
-            index = getKeyIndexWithBinarySearch(-1, new int[]{1, 2, 3});
-            System.out.println(-1 + " index (no element error): " + index);
+            System.out.println("--- Search -2 in {1, 2, 3}: ");
+            index = getKeyIndexWithBinarySearch(-2, new int[]{1, 2, 3});
+            System.out.println("my binary search: " + -2 + " index: " + index);
+            index = Arrays.binarySearch(new int[]{1, 2, 3}, -2);
+            System.out.println("array class binary search: " + -2 + " index: " + index);
 
-            index = getKeyIndexWithBinarySearch(99, array);
-            System.out.println(99 + " index: " + index);
+            System.out.println("--- Search random int: ");
+            int tmp_num = random.nextInt();
+            index = getKeyIndexWithBinarySearch(tmp_num, array);
+            System.out.println("my binary search: " + tmp_num + " index: " + index);
+            index = Arrays.binarySearch(array, tmp_num);
+            System.out.println("array class binary search: " + tmp_num + " index: " + index);
 
+            System.out.println("--- Search 99 in empty array: ");
             index = getKeyIndexWithBinarySearch(99, new int[]{});
             System.out.println(99 + " index: " + index);
         } catch (NullPointerException e) {
